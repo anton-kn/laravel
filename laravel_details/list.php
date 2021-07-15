10b7666c16e97b90e12a8d1eb8de8ac748f4e9a2
anton-kn <antonknjzev@yandex.ru>
2021-07-15 16:52:36 +0400

    Стили для code

--- laravel_details/list.php
+++ laravel_details/list.php
@@ -44,6 +44,20 @@
                         <h2>Тема 1</h2>
                         <p>На данном сайте мы будем писать небольшие заметки о том, каким образом работает фреймворк Laravel и немного о PHP.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                         <pre>
+                             <code>
+$collection = collect([
+    ['name' => 'Regena', 'age' => null],
+    ['name' => 'Linda', 'age' => 14],
+    ['name' => 'Diego', 'age' => 23],
+    ['name' => 'Linda', 'age' => 84],
+]);
+
+$collection->firstWhere('name', 'Linda');
+
+// ['name' => 'Linda', 'age' => 14]
+                             </code>
+                         </pre>
                         
                      </div>
                   </div>
